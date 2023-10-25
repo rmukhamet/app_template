10834046d5f3425c505d614c20f7afda691e9435
Rafael <veliarfl@gmail.com>
2023-10-25 11:00:30 +0300

    Update rules.mk

--- make/rules.mk
+++ make/rules.mk
@@ -6,3 +6,11 @@ generate:
 .PHONY: build
 build: generate
   bin/run-go-build $(foreach name, $(APP_CMD_NAMES), "$(name)")
+
+.PHONY: test
+test: 
+  go test ./...
+
+.PHONY: check
+check:
+  golangci-lint run
