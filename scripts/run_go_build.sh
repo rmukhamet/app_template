120139ca7d78e8ec205a1ae05292f2379eab645e
Rafael <veliarfl@gmail.com>
2023-10-25 11:06:21 +0300

    Update run_go_build.sh

--- scripts/run_go_build.sh
+++ scripts/run_go_build.sh
@@ -1,11 +1,17 @@
 export GO_ENABLED=0
 
+GIT_COMMIT := $(shell git rev-parse HEAD)
+export APP_VERSION=$(RELEASE_VERSION)
+export APP_REVISION=$( GIT COMMIT)
+
 go_build_package() {
   GO_SRC_FILES=$(find "$1" -name "*.go" | tr "\n" " ")
   if [[ "$DEBUG" -eq "1" ]]; then
   go build --gcflags="all=-N -1" -v -o "$2" $GO_SRC_FILES
   else 
-  go build -v -o "$2" $GO_SRC_FILES
+  go build -v -ldflags \
+  '-X version.Version=$APP_VERSION version.Revision=$APP_REVISION' \
+  -o "$2" $GO_SRC_FILES
   fi
   }
 
