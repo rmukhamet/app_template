3ff87dc346a9a8d0bf619e4b1b7554a94ebddede
Rafael <veliarfl@gmail.com>
2023-10-25 10:53:05 +0300

    Update run_go_build.sh

--- scripts/run_go_build.sh
+++ scripts/run_go_build.sh
@@ -1,7 +1,15 @@
+export GO_ENABLED=0
+
 go_build_package() {
   GO_SRC_FILES=$(find "$1" -name "*.go" | tr "\n" " ")
+  if [[ "$DEBUG" -eq "1" ]]; then
+  go build --gcflags="all=-N -1" -v -o "$2" $GO_SRC_FILES
+  else 
+  go build -v -o "$2" $GO_SRC_FILES
+  fi
   }
 
 for APP_NAME in "$@"; do 
 go_build_package "cmd/$APP_NAME" "bin/$APP_NAME"
 done
+
